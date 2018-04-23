b129e3bd4fb816cf9b7ee9d71443d1caa42e89f6
Matt Weldon <matt@mattweldon.com>
2018-04-23 21:04:09 +0100

    Chore: Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const path = require("path");
+
+module.exports = {
+  entry: {
+    app: [
+      './index.js'
+    ]
+  },
+
+  output: {
+    path: path.resolve(__dirname + '/dist'),
+    filename: '[name].js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            { loader: 'css-loader', options: { importLoaders: 1 } },
+            'postcss-loader'
+          ]
+        })
+      },
+      {
+        test:    /\.html$/,
+        exclude: /node_modules/,
+        loader:  'file-loader?name=[name].[ext]',
+      },
+      {
+        test:    /\.elm$/,
+        exclude: [/elm-stuff/, /node_modules/],
+        loader:  'elm-webpack-loader?verbose=true&warn=true',
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader',
+      },
+    ],
+
+    noParse: /\.elm$/,
+  },
+  plugins: [
+    new ExtractTextPlugin('./static/styles.css')
+  ],
+
+  devServer: {
+    inline: true,
+    stats: { colors: true },
+  },
+
+
+};
